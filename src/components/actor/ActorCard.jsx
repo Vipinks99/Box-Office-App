a3ad63c904809613b220bafdd67cb212b4392887
Vipin <vipin.19b131027@abes.ac.in>
2022-07-18 19:18:47 +0530

    Created styles for searched Cards

--- src/components/actor/ActorCard.jsx
+++ src/components/actor/ActorCard.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import { StyledActorCard } from './ActorCard.styled'
 
 const ActorCard = ({ image, name, gender, country, birthday, deathday }) => {
   return (
-    <div>
-      <div>
+    <StyledActorCard>
+      <div className='img-wrapper'>
         <img src={image} alt="actor" />
       </div>
       <h1>
         {name} {gender ? `(${gender})` : null}
       </h1>
       <p>{country ? `Comes from ${country}` : 'No country known'}</p>
-      {birthday ? <p>Born {birthday}</p> : 'Birthday Not Known'}
-      <p>{deathday ? `Died ${deathday}` : 'Alive'}</p>
-    </div>
+      <p>{birthday ? <p>Born {birthday}</p> : 'Birthday Not Known'}</p>
+      <p className='deathday'>{deathday ? `Died ${deathday}` : 'Alive'}</p>
+    </StyledActorCard>
   );
 };
 
