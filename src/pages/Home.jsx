28d392f8fbfa0df967096c5f46dd2d60a3621fa6
Vipin <vipin.19b131027@abes.ac.in>
2022-07-18 13:09:35 +0530

    displayed show result and api fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,55 @@
-import React,{useState} from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+
+import { apiGet } from '../misc/config';
 
 const Home = () => {
+  const [input, setInput] = useState('');
+  const [results, setResult] = useState(null);
 
-  const [input,setInput]=useState('');
-
-  const onInputChange = (ev) =>{
+  const onInputChange = ev => {
     setInput(ev.target.value);
   };
 
-  const onSearch =() =>{
-    // https://api.tvmaze.com/search/shows?q=girls
-
+  const onSearch = () => {
     // To fetch the remote data from API
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result => {
-      console.log(result);
-    })
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResult(result);
+    });
   };
 
-const onKeyDown = (ev) =>{
-  if(ev.keyCode === 13){
-    onSearch();
-  }
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No searched results</div>;
+    }
+
+    if (results && results.length > 0) {
+      return <div>{results.map( (item) => <div key={item.show.id}>{item.show.name}</div>)}</div>
+    };
+
+    return null;
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
+    </MainPageLayout>
+  );
 };
 
-  return ( 
-    <MainPageLayout>
-      <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
-      <button type='button' onClick={onSearch}>Search</button>
-    </MainPageLayout>
-  )
-}
-
-export default Home
+export default Home;
