425681cfefd543e8c96cca427b9019e2c191cd51
Vipin <vipin.19b131027@abes.ac.in>
2022-07-20 23:52:57 +0530

    styled entire app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import Home from './pages/Home';
 import Show from './pages/Show';
 import Starred from './pages/Starred';
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 function App() {
   return (
-    
+    <ThemeProvider theme={theme}>
       <Switch>
         <Route exact path="/">
-          <Home/>
+          <Home />
         </Route>
         <Route exact path="/starred">
-          <Starred/>
+          <Starred />
         </Route>
-        <Route exact path='/show/:id'>
-            <Show />
+        <Route exact path="/show/:id">
+          <Show />
         </Route>
         <Route>This is 404 Page Not found!!</Route>
       </Switch>
+    </ThemeProvider>
   );
 }
 
