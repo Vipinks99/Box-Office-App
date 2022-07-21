05f1f7382934ec5fc3a9593b414b68c822b93f7b
Vipin <vipin.19b131027@abes.ac.in>
2022-07-21 18:33:59 +0530

    Added Animation and complete styling done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { useLocation } from 'react-router';
-import { NavList , LinkStyled} from './Nav.styled';
+import React, { memo } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { NavList, LinkStyled } from './Nav.styled';
 
 const LINKS = [
   { to: '/', text: 'Home' },
@@ -8,17 +9,24 @@ const LINKS = [
 ];
 
 const Nav = () => {
-  const location = useLocation()
+  const location = useLocation();
 
   return (
     <div>
       <NavList>
         {LINKS.map(item => (
-            <li key={item.to}><LinkStyled to={item.to} className={item.to===location.pathname?'active':''}>{item.text}</LinkStyled></li>
+          <li key={item.to}>
+            <LinkStyled
+              to={item.to}
+              className={item.to === location.pathname ? 'active' : ''}
+            >
+              {item.text}
+            </LinkStyled>
+          </li>
         ))}
       </NavList>
     </div>
   );
 };
 
-export default Nav;
+export default memo(Nav);
